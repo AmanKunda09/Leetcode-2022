78477e02d391e2624e6da64479956894550f9e21
AmanKunda09 <80396454+AmanKunda09@users.noreply.github.com>
2022-09-10 13:42:14 +0530

    Time: 2 ms (59.96%), Space: 48.2 MB (76.27%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,22 +1,32 @@
 class Solution {
     public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+        int n=height.length;
+        int left=0,right=n-1;
+        int leftMax=0,rightMax=0;
         int ans=0;
-       int n=height.length;
-        int[] prefix=new int[n];
-        int[] suffix=new int[n];
-        prefix[0]=height[0];
-        for(int i=1;i<n;i++)
+        while(left<=right)
         {
-            prefix[i]=Math.max(height[i],prefix[i-1]);
-        }
-        suffix[n-1]=height[n-1];
-        for(int i=n-2;i>0;i--)
-        {
-            suffix[i]=Math.max(height[i],suffix[i+1]);
-        }
-        for(int i=1;i<n;i++)
-        {
-            ans+=Math.min(prefix[i],suffix[i])-height[i];
+            if(height[left] <= height[right])
+            {
+                if(height[left]>=leftMax)
+                {
+                    leftMax=height[left];
+                }else{
+                    ans+=leftMax-height[left];
+                }
+                left++;
+            }else{
+                if(height[right]>=rightMax)
+                {
+                    rightMax=height[right];
+                }else{
+                    ans+=rightMax-height[right];
+                }
+                right--;
+            }
         }
         return ans;
     }
