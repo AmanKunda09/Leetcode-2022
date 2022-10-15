c43f5a648fd380d2472e8db4f81f0058afbc2889
AmanKunda09 <80396454+AmanKunda09@users.noreply.github.com>
2022-10-16 00:21:24 +0530

    Time: 108 ms (18.97%), Space: 49.2 MB (22.41%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.java
+++ 1531-string-compression-ii/1531-string-compression-ii.java
@@ -17,7 +17,7 @@ class Solution {
                                             dp[l-1][j-del] + 1 + (cnt >= 100 ? 3 : cnt >= 10 ? 2 : cnt >= 2 ? 1: 0));
                 }
                 if (j > 0) // delete s[i]
-                    dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1]);
+                    dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1]); 
             }
         }
         return dp[n][k];
